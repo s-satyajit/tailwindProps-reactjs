d02142b49ad84fc6d1278001db01d02880d35a0b
Satyajit Samal <samalsatyajit668@gmail.com>
2024-07-06 06:17:44 +0530

    Added a new file

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-400 text-black rounded-xl'>Tailwind CSS</h1>
-      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-      <img class="w-24 h-24 rounded-full mx-auto" src="https://images.pexels.com/photos/21033104/pexels-photo-21033104/free-photo-of-tree-with-leaves-and-blossoming-fruit.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="384" height="512"/>
-      <div class="pt-6 space-y-4">
-      <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-      </blockquote>
-      <figcaption class="font-medium">
-        <div class="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div>
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-      </div>
-     </figure>
+      <h1 className='bg-green-400 text-black rounded-xl mb-4'>Tailwind CSS</h1>
+      
+      <Card username="Hitesh Chaudhury" btnText="Click me"/>
+      <Card username="Satyajit Samal"/>
     </>
   )
 }
